7d2ea2ef074d522ae7574128c3304ec925159f7b
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-06-11 21:05:52 -0300

    [feat] db, Tradutor, transformação - Criada as funções para persistencia dos dados ao banco. - Criada funções e classes para manipular o idioma do dado, inserindo no DB o texto traduzido.

---
+++ transform/transform_data.py
@@ -0,0 +1,15 @@
+import db
+from traduzir.Tradutor import Tradutor
+
+
+def transform_data():
+    linhas = db.fetch_results("SELECT * FROM dados where texto_pt is not NULL")
+    for linha in linhas:
+        dados = {
+            "id": linha[0],
+            "texto_en": linha[1]
+        }
+        tradutor = Tradutor(dados["texto_en"], "pt")
+        texto_pt = tradutor.traduzir()
+        db.execute_query('''UPDATE public.dados SET texto_pt= %s WHERE id = %s;''',
+                         params=(texto_pt, dados["id"]))
