1b51797ef5b8b74390ba715a0b1b23b046040d2a
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-06-14 17:23:58 -0300

    [feat] criada aplicação flask para disponibilizar o modelo para consulta

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+
+from modelo.func import predict_text
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/', methods=['GET'])
+def is_fake():
+    try:
+        text = request.get_json()
+
+        if text is None or text == '':
+            return jsonify({'success': False, 'result': 'No texto passed'}), 406
+    except Exception as e:
+        return jsonify({'success': False, 'result': str(e)}), 400
+
+    try:
+        result = predict_text(text)
+    except Exception as e:
+        return jsonify({'success': False, 'result': str(e)}), 422
+
+    return jsonify({'success': False, 'result': result}), 200
